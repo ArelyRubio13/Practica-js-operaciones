03dd2d93c8b0983eeeb28afee951f9571d5e9314
Arely Rubio Gutiérrez <132530243+ArelyRubio13@users.noreply.github.com>
2023-11-06 16:12:05 -0600

    Add files via upload

---
+++ Operaciones/js/cac.js
@@ -0,0 +1,43 @@
+function suma() {
+    let n1, n2, suma
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce otro numero")
+
+    suma = parseInt(n1) + parseInt(n2)
+
+    alert(suma)
+
+}
+
+function resta(){
+    let n1, n2, resta
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce otro numero")
+
+    resta = parseInt(n1) - parseInt(n2)
+
+    alert(resta)
+
+}
+
+function multiplicacion(){
+    let n1, n2, multiplicacion
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce otro numero")
+
+    multiplicacion = parseInt(n1) * parseInt(n2)
+
+    alert(multiplicacion)
+
+}
+
+function division(){
+    let n1, n2, division
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce otro numero")
+
+    division = parseInt(n1) / parseInt(n2)
+
+    alert(division)
+
+}
